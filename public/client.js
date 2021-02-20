e11ed9f54b572b91cfc6f0d1fffb9c0b527fe392
Khan Shariq <khanshariq7654@gmail.com>
2021-02-20 22:14:51 +0530

    NameSpace extraaa

--- public/client.js
+++ public/client.js
@@ -1,9 +1,16 @@
 const socket = io("http://localhost:8080")
 const socket2 = io("http://localhost:8080/main")
 
+socket2.on("connect",()=>{
+    console.log(socket2.id)
+})
 socket.on("connect",()=>{
     console.log(socket.id)
 })
-socket2.on("connect",()=>{
-    console.log(socket2.id)
+
+socket2.on("mainCon",data=>{
+    console.log(data)
+})
+socket.on("welcome",data=>{
+    console.log(data)
 })
--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const io = socketIO(server,{cors:{origin:"*"}});
 app.use(express.static(__dirname + "/public"))
 io.of('/').on("connection",(socket)=>{
     console.log(socket.id)
+    // io.of('/').emit("welcome","emitted")
 })
 io.of('/main').on("connection",(socket)=>{
     console.log(socket.id)
+    io.of('/main').emit("mainCon","main")
+    // io.emit("mainCon","main")
+    io.of('/').emit("welcome","emitted")
+
+    
+
 })
+
