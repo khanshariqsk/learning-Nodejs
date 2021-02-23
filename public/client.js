60f6bd10338dbfdb9a74e1eabe461100a65af7fa
khan shariq <khanshariqsksk@gmail.com>
2021-02-23 16:13:18 +0530

    Rendered Namespaces Dynamically

--- public/client.js
+++ public/client.js
@@ -1,6 +1,16 @@
 const socket = io("http://localhost:8080")
 const socket2 = io("http://localhost:8080/main")
 
-socket.on("joined",(data)=>{
-    console.log(data)
+socket.on("nsData", (nsData) => {
+    document.querySelector('.namespaces').innerHTML = "";
+    nsData.forEach(data => {
+        document.querySelector('.namespaces').innerHTML += `<div class="namespace" ns="${data.endpoint}"><img
+        src="${data.img}" >
+</div>`
+    });
+    document.querySelectorAll(".namespace").forEach((element) => {
+        element.addEventListener('click', (e) => {
+            console.log(element.getAttribute('ns'))
+        })
+    })
 })
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express")
 const app = express()
 const socketIO = require("socket.io")
-
+const nameSpace = require("./data/namespaces")
+// console.log(nameSpace[0])
 const server = app.listen(8080,()=>{
     console.log("Listening to the port 8080.....")
 })
 const io = socketIO(server,{cors:{origin:"*"}});
 app.use(express.static(__dirname + "/public"))
-io.on("connection",(socket)=>{
-    socket.join("level")
-    io.to("level").emit("joined","HelloJI")
+io.of('/').on("connection",(socket)=>{
+   const nsData = nameSpace.map((data)=>{
+       return {
+           img:data.img,
+           endpoint:data.endpoint
+       }
+   })
+   socket.emit("nsData",nsData)
 })
 
 
